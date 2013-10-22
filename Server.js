20e050843c7d657021f8f6030465c2e824d80854
Nils De Moor <nils.de.moor@gmail.com>
2013-10-22 16:11:52 +0200

    jshint code quality check

--- Server.js
+++ Server.js
@@ -1,20 +1,25 @@
-var express = require('express')
-,   webshot = require('webshot')
-,   types   = require('./types.js')
-,   gm      = require('gm')
-,   fs      = require('fs');
+'use strict';
 
-var preRender = fs.readFileSync('./prerender.js').toString();
+var express = require('express'),
+    webshot = require('webshot'),
+    types   = require('./types.js'),
+    gm      = require('gm'),
+    fs      = require('fs');
 
-console.log("Server started at: " + new Date().toUTCString());
+var preRender = fs
+  .readFileSync('./prerender/remove-belowfold-images.js')
+  .toString();
+
+console.log('Server started at: ' + new Date().toUTCString());
 
 var app = express();
 app.set('port', process.env.PORT || 3000);
-app.set('env', process.env.NODE_ENV || 'production')
+app.set('env', process.env.NODE_ENV || 'production');
 
 // Enable logger in development
-if(app.get('env') === 'development')
+if (app.get('env') === 'development') {
   app.use(express.logger('dev'));
+}
 
 // Init middlewares and start server
 app
@@ -22,21 +27,24 @@ app
   .listen(app.get('port'));
 
 // Healthcheck page
-app.get("/healthcheck", function(req, res) {
+app.get('/healthcheck', function (req, res) {
   res.send(200, 'OK!');
 });
 
 // Screenshot pages
-app.get("/:type(iphone|ipad|desktop|custom)", function(req, res) {
-  if(req.query.url === undefined)
+app.get('/:type(iphone|ipad|desktop|custom)', function (req, res) {
+  if (req.query.url === undefined) {
     res.send(500, 'Please provide a url');
+  }
 
-  if(req.query.webhook !== undefined)
-    res.send('Request for "' + req.query.url + '" received')
+  if (req.query.webhook !== undefined) {
+    res.send('Request for "' + req.query.url + '" received');
+  }
 
-  if(req.params.type === 'custom')
+  var options = [];
+  if (req.params.type === 'custom') {
     options = req.params;
-  else {
+  } else {
     options = types[req.params.type];
     options['shotSize'] = { width: 'all', height: 'all' };
     options['timeout'] = req.query.timeout || 20000;
@@ -44,10 +52,10 @@ app.get("/:type(iphone|ipad|desktop|custom)", function(req, res) {
     options['script'] = preRender.replace('%%HEIGHT%%', options.innerHeight);
   }
 
-  webshot(req.query.url, options, function(err, renderStream) {
+  webshot(req.query.url, options, function (err, renderStream) {
     var hasData = false;
 
-    if(err) {
+    if (err) {
       console.log('[ERROR] - ' + req.query.url + ' - webshot: ' + err);
     }
 
@@ -56,38 +64,40 @@ app.get("/:type(iphone|ipad|desktop|custom)", function(req, res) {
       .crop(options.crop.width, options.crop.height)
       .resize(options.resize.width, options.resize.height)
       .stream(function (err, stdout, stderr) {
-        if(err) {
+        if (err) {
           console.log('[ERROR] - ' + req.query.url + ' - resizer: ' + err);
         }
 
-        stderr.on('data', function(data) {
+        stderr.on('data', function (data) {
           console.log(data.toString());
         });
 
-        if(req.query.webhook === undefined) {
-          stdout.on('data', function(data) {
-            if(!hasData) {
+        if (req.query.webhook === undefined) {
+          stdout.on('data', function (data) {
+            if (!hasData) {
               hasData = true;
-              res.contentType("image/png");
+              res.contentType('image/png');
             }
 
             res.write(data);
           });
 
-          stdout.on('end', function(data) {
-            if(!hasData)
+          stdout.on('end', function () {
+            if (!hasData) {
               res.send(500, 'Something went wrong');
-            else
-              res.send()
+            } else {
+              res.send();
+            }
           });
         } else {
           // POST to webhook
           var url = require('url').parse(req.query.webhook);
-          if(url.protocol === 'http:' || url.protocol === 'https:') {
+          if (url.protocol === 'http:' || url.protocol === 'https:') {
             var request = require('request');
-            stdout.pipe(request.post(req.query.webhook, function(err) {
-              if (err) 
-                console.log('[ERROR] - ' + req.query.url + ' - webhook:', err.toString());
+            stdout.pipe(request.post(req.query.webhook, function (err) {
+              if (err) {
+                console.log('[ERROR] - ' + req.query.url + ' - webhook:' + err);
+              }
             }));
           }
         }
--- benchmark.js
+++ benchmark.js
@@ -1,87 +1,92 @@
-var http = require('http')
-  , qs = require('querystring');
+'use strict';
+
+var http = require('http'),
+    qs = require('querystring');
 
 function Benchmark(settings) {
-  if(typeof settings.baseUrl === 'undefined') {
+  if (typeof settings.baseUrl === 'undefined') {
     throw new Error('baseUrl mandatory');
   }
   this.baseUrl = settings.baseUrl;
 
-  if(typeof settings.paths === 'undefined') {
+  if (typeof settings.paths === 'undefined') {
     throw new Error('paths array mandatory');
   }
   this.paths = settings.paths;
 
-  this.port = (typeof settings.port !== 'undefined') ? settings.port : 80;
-  this.timeout = (typeof settings.timeout !== 'undefined') ? settings.timeout : 60*1000;
+  this.port = (typeof settings.port !== 'undefined') ?
+    settings.port : 80;
+  this.timeout = (typeof settings.timeout !== 'undefined') ?
+    settings.timeout : 60 * 1000;
 
-  this.urlSleep = (typeof settings.urlSleep !== 'undefined') ? settings.urlSleep : 10;
-  this.urlSleepSeed = (typeof settings.urlSleepSeed !== 'undefined') ? settings.urlSleepSeed : 0;
-  this.pathSleep = (typeof settings.pathSleep !== 'undefined') ? settings.pathSleep : 1;
+  this.urlSleep = (typeof settings.urlSleep !== 'undefined') ?
+    settings.urlSleep : 10;
+  this.urlSleepSeed = (typeof settings.urlSleepSeed !== 'undefined') ?
+    settings.urlSleepSeed : 0;
+  this.pathSleep = (typeof settings.pathSleep !== 'undefined') ?
+    settings.pathSleep : 1;
 }
 
-Benchmark.prototype.fire = function(urls) {
-  var self = this
-    , seedMin = this.urlSleep - this.urlSleepSeed
-    , seedMax = this.urlSleep + this.urlSleepSeed;
-
-  urls.forEach(function(url, i) {
-    setTimeout(function() { 
-      self._runUrl(url);
-    }, i * Math.floor(Math.random() * (seedMax - seedMin + 1) + seedMin) * 1000);
-  });
-}
-
-Benchmark.prototype._runUrl = function(url) {
-  var self = this;
-
-  this.paths.forEach(function(path, i) {
-    setTimeout(function() { 
-      self._req(url, path, function(msg) {
-        console.log(msg);
-      })
-    }, i * self.pathSleep * 1000);
-  });
-}
-
-Benchmark.prototype._req = function(url, path, callback) {
-  var params = {
-      url: url
-    , timeout: this.timeout
-  }
-  var options = {
-      host: this.baseUrl
-    , port: this.port
-    , path: '/' + path + '?' + qs.stringify(params)
-    , agent: false
-  }
-
-  var self = this
-    , start = Date.now();
-
-  var req = http.request(options, function(res){
-    if (res.statusCode === 200) {
-      callback(self._markupOutput("OK", path, url, start));
-    }
-    else {
-      callback(self._markupOutput("FAIL", path, url, start));
-    }
-  }).on('error', function(error) {
-    callback(self._markupOutput("ERR", path, url, start, new String(error)));
-  }).end();
-}
-
-Benchmark.prototype._markupOutput = function(state, path, url, start, err) {
+function markupOutput(state, path, url, start) {
   var now = Date.now();
 
   return [
-      state
-    , path
-    , url
-    , new Date(start).toLocaleTimeString("en-US", {hour12: false})
-    , new Date(now).toLocaleTimeString("en-US", {hour12: false})
-    , now - start
-  ].join(',')
+    state,
+    path,
+    url,
+    new Date(start).toLocaleTimeString('en-US', {hour12: false}),
+    new Date(now).toLocaleTimeString('en-US', {hour12: false}),
+    now - start
+  ].join(',');
 }
+
+function req(url, path, callback) {
+  var params = {
+    url: url,
+    timeout: this.timeout
+  };
+
+  var options = {
+    host: this.baseUrl,
+    port: this.port,
+    path: '/' + path + '?' + qs.stringify(params),
+    agent: false
+  };
+
+  var start = Date.now();
+
+  http.request(options, function (res) {
+    if (res.statusCode === 200) {
+      callback(markupOutput('OK', path, url, start));
+    }
+    else {
+      callback(markupOutput('FAIL', path, url, start));
+    }
+  }).on('error', function () {
+    callback(markupOutput('ERR', path, url, start));
+  }).end();
+}
+
+function runUrl(url, paths, pathSleep) {
+  paths.forEach(function (path, i) {
+    setTimeout(function () {
+      req(url, path, function (msg) {
+        console.log(msg);
+      });
+    }, i * pathSleep * 1000);
+  });
+}
+
+Benchmark.prototype.fire = function (urls) {
+  var seedMin = this.urlSleep - this.urlSleepSeed;
+  var seedMax = this.urlSleep + this.urlSleepSeed;
+
+  urls.forEach(function (url, i) {
+    setTimeout(function () {
+      runUrl(url, this.paths, this.pathSleep);
+    },
+    i * Math.floor(Math.random() * (seedMax - seedMin + 1) + seedMin) * 1000);
+  });
+};
 
 module.exports = Benchmark;
