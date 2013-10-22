132e02134d86dceb07580dee8ccf7bfe6ef7b613
Nils De Moor <nils.de.moor@gmail.com>
2013-10-22 23:54:18 +0200

    added sleep period to give fixture server time to bind

--- test/Server.js
+++ test/Server.js
@@ -2,13 +2,18 @@
 
 var app = require('../Server.js'),
     express = require('express'),
-    request = require('supertest');
+    request = require('supertest'),
+    sleep = require('sleep');
 
 describe('Routes', function () {
   before(function () {
     // Setup fixture static website
-    app
-      .use(express.static(__dirname + '/fixtures/public'));
+    express()
+      .use(express.static(__dirname + '/fixtures/public'))
+      .listen(4000);
+
+    // Give the fixture server time to bind to the socket
+    sleep.sleep(3);
   });
 
   describe('GET root', function () {
@@ -28,17 +33,9 @@ describe('Routes', function () {
     });
   });
 
-  describe('GET fixture website', function () {
-    it('should return 200', function (done) {
-      request(app)
-        .get('/test.html')
-        .expect(200, done);
-    });
-  });
-
   describe('GET fixture website through loopback', function () {
     it('should return 200', function (done) {
-      request('http://127.0.0.1:3000')
+      request('localhost:4000')
         .get('/test.html')
         .expect(200, done);
     });
