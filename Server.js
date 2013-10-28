c382de4697d205ccf00de5fc681a8cc50ca62f39
Nils De Moor <nils.de.moor@gmail.com>
2013-10-28 18:05:33 +0100

    introduced bunyan logging

--- Server.js
+++ Server.js
@@ -4,27 +4,29 @@ var express = require('express'),
     webshot = require('webshot'),
     types   = require('./types.js'),
     gm      = require('gm'),
-    fs      = require('fs');
+    fs      = require('fs'),
+    bunyan  = require('bunyan'),
+    logger;
 
 var preRender = fs
   .readFileSync('./prerender/remove-belowfold-images.js')
   .toString();
 
-console.log('Server started at: ' + new Date().toUTCString());
-
 var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('env', process.env.NODE_ENV || 'production');
 
-// Enable logger in development
-if (app.get('env') === 'development') {
-  app.use(express.logger('dev'));
-}
+logger = bunyan.createLogger({
+  name: 'shotgun',
+  level: (app.get('env') !== 'test') ? 'info' : 'fatal'
+});
 
 // Init middlewares and start server
 app
   .use(app.router)
   .listen(app.get('port'));
+
+logger.info('Server started at: ' + new Date().toUTCString());
 
 // Healthcheck page
 app.get('/healthcheck', function (req, res) {
@@ -33,6 +35,8 @@ app.get('/healthcheck', function (req, res) {
 
 // Screenshot pages
 app.get('/:type(iphone|ipad|desktop|custom)', function (req, res) {
+  var startTime = Date.now();
+
   if (req.query.url === undefined) {
     res.send(400, 'Please provide a url');
     return;
@@ -57,7 +61,7 @@ app.get('/:type(iphone|ipad|desktop|custom)', function (req, res) {
     var hasData = false;
 
     if (err) {
-      console.log('[ERROR] - ' + req.query.url + ' - webshot: ' + err);
+      logger.error({url: req.query.url, component: 'webshot'}, err);
     }
 
     gm(renderStream)
@@ -66,11 +70,14 @@ app.get('/:type(iphone|ipad|desktop|custom)', function (req, res) {
       .resize(options.resize.width, options.resize.height)
       .stream(function (err, stdout, stderr) {
         if (err) {
-          console.log('[ERROR] - ' + req.query.url + ' - resizer: ' + err);
+          logger.error({url: req.query.url, component: 'resizer'}, err);
         }
 
         stderr.on('data', function (data) {
-          console.log(data.toString());
+          logger.error(
+            {url: req.query.url, component: 'stream'},
+            data.toString()
+          );
         });
 
         if (req.query.webhook === undefined) {
@@ -87,6 +94,11 @@ app.get('/:type(iphone|ipad|desktop|custom)', function (req, res) {
             if (!hasData) {
               res.send(500, 'Something went wrong');
             } else {
+              logger.info({
+                url: req.query.url,
+                time: Date.now() - startTime,
+                type: req.params.type
+              });
               res.send();
             }
           });
@@ -97,7 +109,7 @@ app.get('/:type(iphone|ipad|desktop|custom)', function (req, res) {
             var request = require('request');
             stdout.pipe(request.post(req.query.webhook, function (err) {
               if (err) {
-                console.log('[ERROR] - ' + req.query.url + ' - webhook:' + err);
+                logger.error({url: req.query.url, component: 'webhook'}, err);
               }
             }));
           }
